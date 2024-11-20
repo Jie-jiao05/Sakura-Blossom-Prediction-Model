38dc8ec9c8286ceec4dce0680d04e6b6c3de026d
Jie-jiao05 <jiejiao404@gmail.com>
2024-11-20 17:40:14 -0500

    test

--- scripts/04-test_data.R
+++ scripts/04-test_data.R
@@ -14,10 +14,12 @@ modern_sakura_data <- read_csv(here("data", "02-analysis_data", "analysis_modern
 
 # Test for temperature data (March to May and NA values)
 temperature_test <- all(temperature_data$month %in% c('Mar', 'Apr', 'May'))
+print(temperature_test)
 temperature_na_test <- sum(is.na(temperature_data$mean_temp_c)) == 0
-
+print(temperature_na_test)
 # Test for historical sakura data (NA values)
 historical_sakura_na_test <- sum(is.na(historical_sakura_data)) == 0
-
+print(historical_sakura_na_test)
 # Test for modern sakura data (NA values)
 modern_sakura_na_test <- sum(is.na(modern_sakura_data)) == 0
+print(modern_sakura_na_test)
